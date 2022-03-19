f54f0ff6828d96fcc5105af9872ccf0dca10d172
Jimenezz2 <79206090+Jimenezz2@users.noreply.github.com>
2022-03-19 11:19:38 -0300

    Add files via upload

---
+++ Exercícios/Exercício_4.ino
@@ -0,0 +1,17 @@
+const int botao = 2;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+}
+
+int var;
+
+void loop()
+{
+  var = digitalRead(botao);
+  if (var){
+    digitalWrite (3, HIGH);
+  }
+  }
